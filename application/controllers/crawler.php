a07f1d93a57cc74f31447a9480a7f7296dec849e
Przemyslaw Bobak <bobak.art@gmail.com>
2011-11-14 15:51:42 +0000

    cookie + check in db for existing ones

--- application/controllers/crawler.php
+++ application/controllers/crawler.php
@@ -16,6 +16,125 @@ class Crawler extends CI_Controller {
 		$this->load->model('tags_model');
 		$this->load->model('cookies_model');
 	}
+
+	public function url(){
+	
+	// Setting null arrays
+	$data = array();
+	$logs = array();
+	
+		// Get profiles
+		$logs[] = array('item'=>'Skrypt', 'message'=>'Uruchomiono skrypt');
+		$data['profiles'] = $this->profiles_model->get();
+		
+			foreach( $data['profiles'] as $profile_row ){
+			
+				$logs[] = array('item' => 'Profil: '.$profile_row->name , 'message' => 'Otworzono profil');
+				
+				// Profiles linking
+				//
+				$this->urls_model->idProfiles = $profile_row->id;
+				$this->cookies_model->idProfiles = $profile_row->id;
+				$this->tags_model->idProfiles = $profile_row->id;
+				
+				
+				// Get data according to profile
+				//
+				$data['urls'] = $this->urls_model->get_by_profile();
+				$cookies = $this->cookies_model->get_by_profile(1);
+				$tags = $this->tags_model->get_by_profile();
+				
+				// /src=[\"']?([^www.megavideo.com/v/?.*])[\"']/i
+				///href=[\'"]?[^www.megavideo.com$]([^\'" >]+)[\'" >]/
+				// /src=[\"']?([^\"']?.*(png|jpg|gif))[\"']/i
+				//  src="http://www.megavideo.com/v/N6EL9OXV"
+				// Each url > parse it with tags' patterns	
+				if(isset($data['urls']) and $data['urls']) {
+					
+					foreach( $data['urls'] as $url_row ){
+				
+						$urla = $url_row->url;
+						
+						//$cookie = $cookies->
+						// COOKIE > SET VALUES > EXEC > RETURN FILE
+						
+						
+						if(  startsWith($urla,'/') ){
+							$urla = $profile_row->url . $urla;
+						}
+						$adres = prep_url($urla);
+						
+						// LOAD PAGE INTO VAR WITH COOKIE
+						$c = curl_init($adres);
+						curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+						//curl_setopt($c, CURLOPT_COOKIE, $cookie);9im
+						//curl_setopt($c, CURLOPT_HEADER, TRUE);
+						curl_setopt($c, CURLOPT_USERAGENT, 'Googlebot/2.1 (+http://www.googlebot.com/bot.html)');
+						$result_page = curl_exec($c);
+						curl_close($c);
+						//echo $result_page;
+
+						$logs[] = array('item' => 'Adres URL', 'message' => $urla);
+						
+
+						foreach($tags as $tag_row){
+						
+							//$tag_text = addslashes( str_replace('(:any)', '(.*)' ,$tag_row->text) );
+							$tag_text = $tag_row->text;
+							switch($tag_row->type){
+								
+								case 'url' :  
+								
+									preg_match_all($tag_text, $result_page, $base);
+									//[0] = "ALL"
+									// [1] = " CONENT ( VAR 1 )"
+									$bas = $base[1];
+									//print_r($bas);
+									foreach($bas as $row){
+										if(isset($row) and $row<>'' and $row<>'#' and $row<>'/'){
+									
+												$this->urls_model->id = '';
+												$this->urls_model->idProfiles = $profile_row->id;
+												$this->urls_model->url = $row;
+												$this->urls_model->text = $row;
+												$this->urls_model->save();
+												
+												//print_r(htmlspecialchars($row));
+												$logs[] = array('item'=>'Adres URL' , 'message' => 'Dodany nowy adres URL.');
+												//unset($base);
+										}
+									}
+									break;
+							
+							}
+
+						}
+						
+						unset($result_page);
+					
+					}
+					
+				}else{
+				
+					$logs[] = array('item'=>'Adres URL' , 'message' => 'Brak adresów URL');
+				
+				}
+		
+			}
+		
+		$logs[] = array('item'=>'Skrypt', 'message'=>"Skrypt zakończył działanie w czasie: {elapsed_time}s");
+	
+		$this->users->id = $this->session->userdata('id');
+		$data['userdata'] = $this->users->get_by_id();
+		
+		$data['logs'] = $logs;	
+		
+		$this->load->view('crawler',$data);
+		
+	
+	
+	}
+	
 	public function index(){
 	
 	// Setting null arrays
@@ -79,9 +198,22 @@ class Crawler extends CI_Controller {
 						// LOAD PAGE INTO VAR WITH COOKIE
 						$c = curl_init($adres);
 						curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-						//curl_setopt($c, CURLOPT_COOKIE, $cookie);
-						//curl_setopt($c, CURLOPT_HEADER, TRUE);
-						curl_setopt($c, CURLOPT_USERAGENT, 'Googlebot/2.1 (+http://www.googlebot.com/bot.html)');
+						if(isset($cookies) and $cookies){
+							//$cookie = CREATE COOKIE
+							
+								$cookie = 'cookie';
+								$ch = curl_init($cookies->url);
+								curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+								curl_setopt($ch, CURLOPT_USERAGENT, $cookies->agent);
+								curl_setopt ($ch, CURLOPT_HEADER, 0);
+								curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie);
+								curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
+								curl_setopt($ch, CURLOPT_POST, true);
+								curl_setopt($ch, CURLOPT_POSTFIELDS,$cookies->content);
+								$resuslti = curl_exec ($ch);
+								curl_close ($ch);
+						}
+						
 						$result_page = curl_exec($c);
 						curl_close($c);
 						//echo $result_page;
@@ -91,7 +223,6 @@ class Crawler extends CI_Controller {
 
 						foreach($tags as $tag_row){
 						
-							//$tag_text = addslashes( str_replace('(:any)', '(.*)' ,$tag_row->text) );
 							$tag_text = $tag_row->text;
 							switch($tag_row->type){
 							
@@ -99,7 +230,8 @@ class Crawler extends CI_Controller {
 									preg_match($tag_text, $result_page, $base_title);
 									print_r($base_title)."\n";
 									if(isset($base_title) and $base_title and $base_title[1]<>''){
-										$video = $base_title[1];
+										$videos = $base_title[1];
+										$video = substr(parse_url($videos, PHP_URL_PATH), 3, 8);
 										//print_r($base_title);
 										$logs[] = array('item'=>'Video' , 'message' => 'Znaleziono i dodano wideo.');
 									}else{
@@ -107,6 +239,7 @@ class Crawler extends CI_Controller {
 									}
 								break;
 								
+								// SAVED FOR SEPARATE METHOD
 								/*case 'url' :  
 								
 									preg_match_all($tag_text, $result_page, $base);
@@ -156,7 +289,7 @@ class Crawler extends CI_Controller {
 								
 								case 'img' :
 									preg_match($tag_text, $result_page, $base_img);
-									print_r($base_img)."\n";
+								//	print_r($base_img)."\n";
 									if(isset($base_img) and $base_img){
 										$img = $base_img[1];										
 										$logs[] = array('item'=>'Obrazek' , 'message' => 'Znaleziono i dodano obrazek.');
@@ -177,16 +310,29 @@ class Crawler extends CI_Controller {
 						
 						
 		// <img []>
-							
-		/*$sql_d = "SELECT `id` FROM `dle_category` WHERE `alt_name`='".$cat['2'][$e]."' LIMIT 1";
-							
-							$sql = "INSERT INTO dle_post SET autor='".$autor[rand(1, 10)]."',
-		category='".$id_cat."', date='".$data."',
+		
+		if(isset($video) and $video<>''){
+			$dats = date('YmdHis');
+			if(!isset($img)){
+				$img = ' ';
+			}
+			$xfileds = 'image|'.$img.'||jakosc|Dobra||jezyk|Brak Danych||rok|Brak Danych';			
+			
+			$this->db->where('full_story',$video);
+			$qury = $this->db->get('dle_post',1);
+			
+			if($qury->num_rows()==0){
+			
+			
+					$sql = "INSERT INTO dle_post SET 
+							autor='".$autor[rand(1, 10)]."',
+		category='1', 
+		date='".$dats."',
 		title='".$title."',
 		alt_name='".url_title($title)."',
 		short_story='$opis',
-		full_story='$megavideo',
-		xfields='$xfileds',
+		full_story='".$video."',
+		xfields='".$xfileds."',
 		comm_num='0',
 		allow_comm='1', 
 		allow_main='1',
@@ -202,7 +348,12 @@ class Crawler extends CI_Controller {
 		votes='0', 
 		view_edit='0', 
 		flag='1'
-		";*/
+		";
+		
+				$this->db->query($sql);
+		}
+		
+		}
 					
 					}
 					
